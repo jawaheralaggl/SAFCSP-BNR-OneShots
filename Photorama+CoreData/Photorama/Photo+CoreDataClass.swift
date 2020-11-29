edbf73d9d7bf2e8a5bdebc55043401f4b784a623
jawaheralaggl <jawaheralaggl@gmail.com>
2020-11-29 13:13:12 +0300

    chapter listing

---
+++ Photorama+CoreData/Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Jawaher Alagel on 11/29/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
